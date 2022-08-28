c48bbe98229d40f46bc2330400c7522a0a4dc234
Joonhyuk Choi <joonhyuk@me.com>
2022-08-28 22:17:05 +0900

    refactoring object structure, remove deprecated stuffs

--- lib/escape/actor.py
+++ lib/escape/actor.py
@@ -88,62 +88,11 @@ class EscapePlayer(Character):
             sprite_first_hit.color = colors.RED
             print(sprite_first_hit.owner)
 
-    
-    
-class OldPlayer(Actor):
-    
-    def __init__(self, physics_engine: PhysicsEngine = None, **kwargs) -> None:
-        super().__init__(sprite = Sprite(IMG_PATH + 'player_handgun_original.png'), 
-                         size = Vector(32, 32), 
-                         physics_engine = physics_engine, 
-                         mass = 1, 
-                         body_type = physics_types.dynamic, 
-                         collision_type = collision.character, 
-                         physics_shape = physics_types.circle, 
-                         **kwargs)
-        self.hp = 100
-        self.camera = CameraHandler()
-        self.movement = PhysicsMovement()
 
-    def tick(self, delta_time: float = None) -> bool:
-        if not super().tick(delta_time): return False
-        direction = ENV.direction_input
-        if direction: self.movement.turn_toward(direction)
-        self.movement.move(ENV.move_input)
-        ENV.debug_text['player_speed'] = round(self.speed, 1)
-        
-    def apply_damage(self, damage:float):
-        self.hp -= damage
+class Door(Pawn):
     
-    def test_directional_attack(self, 
-                                target_direction:Vector = None, 
-                                thickness = 1.0,
-                                distance = 500,
-                                muzzle_speed:float = 300,
-                                ):
-        origin = self.position
-        if not target_direction: target_direction = Vector.directional(self.angle)
-        end = target_direction * distance + origin
-        self.body.physics.shape.filter = pymunk.ShapeFilter(categories=0b1)
-        shape_filter = pymunk.ShapeFilter(mask = pymunk.ShapeFilter.ALL_MASKS()^0b1)
-        
-        query = ENV.physics_engine.space.segment_query(origin, end, thickness / 2, shape_filter)
-        
-        if query:
-            first_hit = query[0]
-            sprite_first_hit:Sprite = ENV.physics_engine.get_object_from_shape(first_hit.shape)
-            sprite_first_hit.color = colors.RED
-            print(sprite_first_hit.owner)
-    
-    @property
-    def is_alive(self) -> bool:
-        if self.hp <= 0: return False
-        return super().is_alive
-
-
-class Door(Actor2D):
-    def __init__(self, body: Sprite = None, hp:float = 1000, **kwargs) -> None:
-        super().__init__(body, **kwargs)
+    def __init__(self, body: DynamicBody, hp: float = 1000, **kwargs) -> None:
+        super().__init__(body, hp, **kwargs)
         self.hp = hp
         self._open = False
         self._locked = False
--- lib/foundation/engine.py
+++ lib/foundation/engine.py
@@ -436,104 +436,6 @@ class ActionComponent(ActorComponent):
     pass
 
 
-class Actor(BaseActor):
-    """ LEGACY : WILL BE DEPRECATED """
-    """ Actor that have world presence like body, position """
-    def __init__(self, 
-                 sprite:Sprite,
-                 size:Vector = None,
-                 physics_engine:PhysicsEngine = None,
-                 mass:float = 1.0,
-                 body_type:int = None,
-                 collision_type:int = None,
-                 elasticity:float = None,
-                 friction:float = 0.2,
-                 shape_edge_radius:float = 0.0,
-                 physics_shape = None,
-                 name:Optional[str] = None,
-                 **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.body:Body = Body(sprite, 
-                              size, 
-                              physics_engine, 
-                              mass, body_type, 
-                              collision_type, 
-                              elasticity, 
-                              friction, 
-                              shape_edge_radius, 
-                              physics_shape)
-    
-    def spawn(self, 
-              position:Vector = vectors.zero, 
-              angle:float = 0.0, 
-              velocity:Vector = None,
-              visibility:bool = True,
-              lifetime:float = 0.0
-              ):
-        # self.register_components()
-        
-        self.body.spawn()
-        self.position = position
-        self.angle = angle
-        
-        if velocity: self.velocity = velocity
-        if not visibility: self.visibility = False
-        return super().spawn(lifetime)
-    
-    def tick(self, delta_time:float = None) -> bool:
-        if delta_time is None: delta_time = ENV.delta_time
-        if not super().tick(delta_time): return False
-        return True
-    
-    def draw(self):
-        self.body.draw()
-    
-    def _get_visibility(self) -> bool:
-        return self.body.visibility
-    
-    def _set_visibility(self, switch:bool):
-        self.body.visibility = switch
-    
-    visibility:bool = property(_get_visibility, _set_visibility)
-    
-    def _get_position(self) -> Vector:
-        return self.body.position
-    
-    def _set_position(self, position) -> None:
-        self.body.position = position
-    
-    position:Vector = property(_get_position, _set_position)
-    
-    def _get_angle(self) -> float:
-        return self.body.angle
-    
-    def _set_angle(self, angle:float):
-        self.body.angle = angle
-    
-    angle:float = property(_get_angle, _set_angle)
-    
-    def _get_velocity(self) -> Vector:
-        return self.body.velocity
-    
-    def _set_velocity(self, velocity):
-        self.body.velocity = velocity
-    
-    velocity:Vector = property(_get_velocity, _set_velocity)
-    
-    @property
-    def screen_position(self) -> Vector:
-        ''' relative position in viewport '''
-        return self.position - ENV.abs_screen_center + CONFIG.screen_size / 2
-    
-    @property
-    def forward_vector(self) -> Vector:
-        return self.body.forward_vector
-
-    @property
-    def speed(self) -> float:
-        return self.body.speed
-
-
 class BodyComponent(ActorComponent):
     __slots__ = ('sprite', 
                  '_hidden', 
@@ -547,9 +449,14 @@ class BodyComponent(ActorComponent):
         super().__init__(**kwargs)
         
         self.sprite:Sprite = sprite
+        self._hidden:bool = False
+        
         if position: self.position = position
         if angle: self.angle = angle
         # self.get_ref() = self.sprite
+    
+    # def on_register(self):
+    #     print('body component set',self)
     
     def get_ref(self):
         return self.sprite
@@ -570,6 +477,12 @@ class BodyComponent(ActorComponent):
     
     def draw(self, *args, **kwargs):
         self.sprite.draw(*args, **kwargs)
+    
+    def hide(self, switch:bool = None):
+        ''' hide sprite and physics body '''
+        if switch is None: switch = not self._hidden
+        self.visibility = switch
+        pass
     
     def destroy(self) -> bool:
         self.sprite.remove_from_sprite_lists()
@@ -615,6 +528,11 @@ class BodyComponent(ActorComponent):
     
     @property
     def speed(self) -> float :
+        ''' different from physics or sprite 
+        
+        :physics = per sec
+        :sprite = per tick
+        '''
         return self.velocity.length
     
     @property
@@ -959,267 +877,6 @@ class DynamicObject(BaseActor):
     def speed(self) -> float:
         return self.body.speed
         
-
-class Body(ActorComponent):
-    """ LEGACY : WILL BE DEPRECATED """
-    '''
-    has size, sprite for draw, move(collision), hit(collision)
-    based on pymunk physics engine
-    가능한 엔진에 종속되지 않도록.
-    '''
-    
-    def __init__(self, 
-                 sprite:Sprite, 
-                 size:Vector = None,
-                 physics_engine:PhysicsEngine = None,
-                 mass:float = 1.0,
-                 body_type:int = None,
-                 collision_type:int = None,
-                 elasticity:float = None,
-                 friction:float = None,
-                 shape_edge_radius:float = 0.0,
-                 physics_shape = None,
-                 ) -> None:
-        super().__init__()
-        self.sprite:Sprite = None
-        ''' for draw. should be expanded for attachment and vfx '''
-        self.physics:PhysicsObject = None
-        ''' for move, hit check
-        body : [mass, moment, type(STATIC, DYNAMIC, KINEMATIC)]
-        shape : [body, size or vertices, radius, collision_type, elasticity, friction]
-        hitbox : use shape or sprite poly
-        '''
-        self._physics_engine:PhysicsEngine = None
-        self.mass_mul:float = 1.0
-        self.mass_add:float = 0.0
-        self.friction_mul:float = 1.0
-        self.friction_add:float = 0.0
-        self.elasticity_mul:float = 1.0
-        self.elasticity_add:float = 0.0
-        
-        if physics_shape is physics_types.circle:
-            rad = max(size.x, size.y) / 2
-            shape = physics_types.circle(body = None, radius = rad)
-            moment = pymunk.moment_for_circle(mass, 0, rad)
-            # moment = PhysicsEngine.MOMENT_INF
-        else:
-            shape = None
-            moment = None
-        self.setup(sprite = sprite, 
-                   physics_engine = physics_engine, 
-                   mass = mass, 
-                   moment = moment, 
-                   body_type = body_type, 
-                   shape = shape, 
-                   collision_type = collision_type, 
-                   elasticity = elasticity, 
-                   friction = friction, 
-                   shape_edge_radius = shape_edge_radius
-                   )
-    
-    def setup(self,
-              sprite:Sprite,
-              physics_engine:PhysicsEngine = None,
-              mass:float = 1,
-              moment:float = 0,
-              body_type:int = None,
-              shape:physics_types.shape = None,
-              collision_type:int = collision.default,
-              elasticity:float = None,
-              friction:float = 0.2,
-              shape_edge_radius:float = 0.0,
-              ):
-        self.sprite = sprite
-        self._physics_engine = physics_engine
-        if self._physics_engine:
-            self.physics = self._physics_engine.add_sprite(sprite = self.sprite,
-                                                           mass = mass,
-                                                           moment_of_inertia = moment,
-                                                           body_type = body_type,
-                                                           shape = shape,
-                                                           elasticity = elasticity,
-                                                           friction = friction,
-                                                           radius = shape_edge_radius,
-                                                           collision_type = collision_type,
-                                                           spawn = False,
-                                                           )
-        
-    @property
-    def has_physics(self) -> bool:
-        if self._physics_engine: return True
-        return False
-    
-    def spawn(self, object_layer:ObjectLayer = None, physics_instance:PhysicsEngine = None):
-        if self.has_physics:
-            self._physics_engine.add_to_space(self.sprite)
-        if object_layer:
-            object_layer.add(self.sprite)
-        return super().spawn()
-    
-    def despawn(self):
-        self.remove()
-    
-    def remove(self):
-        if self.has_physics: self._physics_engine.remove_sprite(self.sprite)
-        if self.sprite: self.sprite.remove_from_sprite_lists()
-        # if self.physics: self.physics
-        # if self.hit_collision: self.hit_collision.remove_from_sprite_lists()
-    
-    def draw(self):
-        self.sprite.draw()
-        if CONFIG.debug_draw: 
-            self.physics.draw()
-    
-    def apply_force_local(self, force:Vector = vectors.zero):
-        if not self.has_physics: self.velocity += force / self.physics.body.mass
-        else: return self.physics.body.apply_force_at_local_point(force)
-    
-    def apply_impulse_local(self, impulse:Vector = vectors.zero):
-        if not self.has_physics: PhysicsException('Can\'t apply impulse to non-physics object')
-        return self.physics.body.apply_impulse_at_local_point(impulse)
-    
-    def apply_force_world(self, force:Vector = vectors.zero):
-        if not self.has_physics: self.velocity += force / self.physics.body.mass    ### should revisit later
-        return self.physics.body.apply_force_at_world_point(force, self.position)
-    
-    def apply_impulse_world(self, impulse:Vector = vectors.zero):
-        if not self.has_physics: PhysicsException('Can\'t apply impulse to non-physics object')
-        return self.physics.body.apply_impulse_at_world_point(impulse, self.position)
-    
-    def apply_acceleration_world(self, acceleration:Vector):
-        self.apply_force_world(acceleration * self.mass)
-    
-    def sync(self):
-        ''' mostly not used manually '''
-        if not self.physics: return False
-        if self.physics.body.is_sleeping: return False
-        
-        pos_diff = self.position - self.sprite.position
-        ang_diff = self.angle - self.sprite.angle
-        
-        self.sprite.position = self.position
-        self.sprite.angle = self.angle
-        
-        self.sprite.pymunk_moved(self.physics, pos_diff, ang_diff)
-        return True
-    
-    def _get_visibility(self) -> bool:
-        return self.sprite.visible
-    
-    def _set_visibility(self, switch:bool):
-        self.sprite.visible = switch
-    
-    visibility:bool = property(_get_visibility, _set_visibility)
-        
-    def _get_position(self) -> Vector:
-        if self.physics:
-            return Vector(self.physics.position)
-        return Vector(self.sprite.position)
-    
-    def _set_position(self, position) -> None:
-        if self.physics:
-            self.physics.position = position
-        else: self.sprite.position = position
-    
-    position:Vector = property(_get_position, _set_position)
-    
-    def _get_angle(self) -> float:
-        if self.physics:
-            return self.physics.angle
-        return self.sprite.angle
-    
-    def _set_angle(self, angle:float = 0.0):
-        if self.physics:
-            self.physics.angle = angle
-        else: self.sprite.angle = angle
-    
-    angle:float = property(_get_angle, _set_angle)
-    
-    def _get_velocity(self) -> Vector:
-        if self.physics:
-            return Vector(self.physics.body.velocity)
-        return Vector(self.sprite.velocity)
-    
-    def _set_velocity(self, velocity):
-        if self.physics:
-            self.physics.body.velocity = velocity   ### physics.body.velocity is tuple
-        else: self.sprite.velocity[:] = velocity[:] ### trivial optimization for list
-    
-    velocity:Vector = property(_get_velocity, _set_velocity)
-    
-    def _get_mass(self):
-        return self.physics.body.mass * self.mass_mul + self.mass_add
-    
-    def _set_mass(self, mass:float):
-        self.physics.body.mass = mass
-    
-    mass:float = property(_get_mass, _set_mass)
-    
-    def _get_damping(self):
-        return self.sprite.pymunk.damping
-    
-    def _set_damping(self, damping:float):
-        self.sprite.pymunk.damping = damping
-    
-    damping:float = property(_get_damping, _set_damping)
-    
-    def _get_max_speed(self):
-        return self.sprite.pymunk.max_velocity
-    
-    def _set_max_speed(self, max_speed:int):
-        self.sprite.pymunk.max_velocity = max_speed
-    
-    max_speed:int = property(_get_max_speed, _set_max_speed)
-    
-    def _get_friction(self):
-        return self.physics.shape.friction
-    
-    def _set_friction(self, friction:float):
-        self.physics.shape.friction = friction
-    
-    friction:float = property(_get_friction, _set_friction)
-    """
-    쿨롱 마찰력
-    
-        Some real world example values from Wikipedia (Remember that
-        it is what looks good that is important, not the exact value).
-
-        ==============  ======  ========
-        Material        Other   Friction
-        ==============  ======  ========
-        Aluminium       Steel   0.61
-        Copper          Steel   0.53
-        Brass           Steel   0.51
-        Cast iron       Copper  1.05
-        Cast iron       Zinc    0.85
-        Concrete (wet)  Rubber  0.30
-        Concrete (dry)  Rubber  1.0
-        Concrete        Wood    0.62
-        Copper          Glass   0.68
-        Glass           Glass   0.94
-        Metal           Wood    0.5
-        Polyethene      Steel   0.2
-        Steel           Steel   0.80
-        Steel           Teflon  0.04
-        Teflon (PTFE)   Teflon  0.04
-        Wood            Wood    0.4
-        ==============  ======  ========
-
-    """
-    
-    @property
-    def forward_vector(self) -> Vector:
-        return vectors.right.rotate(self.angle)
-    
-    @property
-    def speed(self):
-        ''' different from physics or sprite 
-        
-        :physics = per sec
-        :sprite = per tick
-        '''
-        return self.velocity.length
-
 
 class App(arcade.Window):
     
