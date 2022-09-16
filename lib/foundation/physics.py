b59db6ccb705c41faad088cdb1415aad55cba7de
Joonhyuk Choi <joonhyuk@me.com>
2022-09-16 14:37:18 +0900

    refactoring, bugfix

--- lib/foundation/physics.py
+++ lib/foundation/physics.py
@@ -9,114 +9,11 @@ from typing import Callable, Optional, Union
 from enum import Enum
 
 ### for in-house physics engine
-import pymunk, math
+import arcade, pymunk, math
 from .vector import Vector
 from config.engine import *
 
-from .utils import debug_draw_poly
-### for in-house physics engine
-
-### for code injection of simple physics engine
-# will be deprecated after moved to pymunk
-import arcade
-from arcade import (Point, 
-                    PointList, 
-                    is_point_in_polygon, 
-                    get_distance, 
-                    Sprite, 
-                    are_polygons_intersecting
-                    )
-### for code injection of simple physics engine
-
-def is_polygon_intersecting_with_circle(poly: PointList, p: Point, radius: float):  ### function added to arcade simple physics engine
-    ''' for legacy simple physics engine '''
-    if is_point_in_polygon(*p, poly): return True   # 중심이 안에 있으면 충돌
-    
-    for i in range(-len(poly), 0):
-        if get_distance(*poly[i], *p) <= radius: return True    # 모서리 충돌
-        
-        p1 = poly[i]
-        p2 = poly[i + 1]
-
-        # v12 = (p2[0] - p1[0], p2[1] - p1[1])
-        # v21 = (p1[0] - p2[0], p1[1] - p2[1])
-        # v1 = (p[0] - p1[0], p[1] - p1[1])
-        # v2 = (p[0] - p2[0], p[1] - p2[1])
-
-        # def dot(a:tuple, b:tuple):
-        #     return sum(i * j for i, j in zip(a, b))
-        
-        dot1 = (p2[0] - p1[0]) * (p[0] - p1[0]) + (p2[1] - p1[1]) * (p[1] - p1[1]) >= 0
-        dot2 = (p2[0] - p1[0]) * (p[0] - p2[0]) + (p2[1] - p1[1]) * (p[1] - p2[1]) < 0
-        
-        # if dot(v12, v1) >=0 and dot(v21, v2) >=0:
-        if dot1 and dot2:
-            projection = ((p2[0] - p1[0]) * (p1[1] - p[1]) - (p1[0] - p[0]) * (p2[1] - p1[1])) / get_distance(*p1, *p2)
-        
-        # if 0 < projection <= d12:
-            if abs(projection) <= radius:
-                # print('sides check [colliding]')
-                return True
-            # print('sides check [not colliding]')
-            
-        # if projection <= radius: 
-        #     print(p, p1, p2, d12, projection)
-        #     return True
-        
-    
-    return False
-
-def is_sprite_intersecting_with_line(start:tuple, end:tuple, sprite:arcade.Sprite):
-    
-    pass
-
-def _check_for_collision(sprite1: Sprite, sprite2: Sprite) -> bool:
-    """
-    Check for collision between two sprites.
-
-    :param Sprite sprite1: Sprite 1
-    :param Sprite sprite2: Sprite 2
-
-    :returns: True if sprites overlap.
-    :rtype: bool
-    """
-    collision_radius_sum = sprite1.collision_radius + sprite2.collision_radius
-
-    diff_x = sprite1.position[0] - sprite2.position[0]
-    diff_x2 = diff_x * diff_x
-
-    if diff_x2 > collision_radius_sum * collision_radius_sum:
-        return False
-
-    diff_y = sprite1.position[1] - sprite2.position[1]
-    diff_y2 = diff_y * diff_y
-    if diff_y2 > collision_radius_sum * collision_radius_sum:
-        return False
-
-    distance = diff_x2 + diff_y2
-    if distance > collision_radius_sum * collision_radius_sum:
-        return False
-
-    ### mash custom starts
-    if getattr(sprite1, 'collides_with_radius', False):
-        if getattr(sprite2, 'collides_with_radius', False):
-            # print('collision case 0')
-            return True
-        else:
-            # print('collision case 1')
-            return is_polygon_intersecting_with_circle(sprite2.get_adjusted_hit_box(), sprite1.position, sprite1.collision_radius)
-    else:
-        if getattr(sprite2, 'collides_with_radius', False):
-            # print('collision case 2')
-            return is_polygon_intersecting_with_circle(sprite1.get_adjusted_hit_box(), sprite2.position, sprite2.collision_radius)
-    ### mash custom ends
-    
-    return are_polygons_intersecting(
-        sprite1.get_adjusted_hit_box(), sprite2.get_adjusted_hit_box()
-    )
-
-### code injection
-arcade.sprite_list.spatial_hash._check_for_collision = _check_for_collision
+from arcade import Sprite
 
 def is_sprite_not_in_zone(sprite:Sprite):
     
@@ -321,16 +218,16 @@ class PhysicsObject:
     
     def draw(self):
         if not CONFIG.debug_draw: return False
-        if isinstance(self.shape, physics_types.circle):
-            arcade.draw_circle_outline(*self.body.position, self.shape.radius, (255,255,255,255))
-        else:
-            if isinstance(self.shape, physics_types.poly):
-                shape = []
-                angle = self.body.angle
-                points = self.shape.get_vertices()
-                for point in points:
-                    shape.append(point.rotated(angle))
-                debug_draw_poly(self.body.position, shape, (255,255,255,255))
+        # if isinstance(self.shape, physics_types.circle):
+        #     arcade.draw_circle_outline(*self.body.position, self.shape.radius, (255,255,255,255))
+        # else:
+        #     if isinstance(self.shape, physics_types.poly):
+        #         shape = []
+        #         angle = self.body.angle
+        #         points = self.shape.get_vertices()
+        #         for point in points:
+        #             shape.append(point.rotated(angle))
+        #         debug_draw_poly(self.body.position, shape, (255,255,255,255))
     
     def _get_scale(self):
         return self._scale
@@ -882,7 +779,8 @@ class PhysicsEngine:
                 continue
 
             if is_sprite_not_in_zone(sprite):
-                sprite.owner.destroy()
+                if hasattr(sprite, 'owner'): sprite.owner.destroy()
+                else: sprite.remove_from_sprite_lists()
                 continue
 
             original_position = sprite.position
--- lib/foundation/utils.py
+++ lib/foundation/utils.py
@@ -1,6 +1,7 @@
 '''
 utils coupled with arcade, pymunk, pyglet, etc
 
+...and some injections
 '''
 import functools
 
@@ -10,6 +11,114 @@ from lib.foundation.base import *
 from config.engine import *
 from pyglet.clock import schedule, schedule_once, schedule_interval, schedule_interval_soft, unschedule
 from pyglet.clock import _default as pyglet_clock
+
+import pyglet
+
+### for in-house physics engine
+
+### for code injection of simple physics engine
+# will be deprecated after moved to pymunk
+import arcade
+from arcade import (Point, 
+                    PointList, 
+                    is_point_in_polygon, 
+                    get_distance, 
+                    Sprite, 
+                    are_polygons_intersecting
+                    )
+### for code injection of simple physics engine
+
+def is_polygon_intersecting_with_circle(poly: PointList, p: Point, radius: float):  ### function added to arcade simple physics engine
+    ''' for legacy simple physics engine '''
+    if is_point_in_polygon(*p, poly): return True   # 중심이 안에 있으면 충돌
+    
+    for i in range(-len(poly), 0):
+        if get_distance(*poly[i], *p) <= radius: return True    # 모서리 충돌
+        
+        p1 = poly[i]
+        p2 = poly[i + 1]
+
+        # v12 = (p2[0] - p1[0], p2[1] - p1[1])
+        # v21 = (p1[0] - p2[0], p1[1] - p2[1])
+        # v1 = (p[0] - p1[0], p[1] - p1[1])
+        # v2 = (p[0] - p2[0], p[1] - p2[1])
+
+        # def dot(a:tuple, b:tuple):
+        #     return sum(i * j for i, j in zip(a, b))
+        
+        dot1 = (p2[0] - p1[0]) * (p[0] - p1[0]) + (p2[1] - p1[1]) * (p[1] - p1[1]) >= 0
+        dot2 = (p2[0] - p1[0]) * (p[0] - p2[0]) + (p2[1] - p1[1]) * (p[1] - p2[1]) < 0
+        
+        # if dot(v12, v1) >=0 and dot(v21, v2) >=0:
+        if dot1 and dot2:
+            projection = ((p2[0] - p1[0]) * (p1[1] - p[1]) - (p1[0] - p[0]) * (p2[1] - p1[1])) / get_distance(*p1, *p2)
+        
+        # if 0 < projection <= d12:
+            if abs(projection) <= radius:
+                # print('sides check [colliding]')
+                return True
+            # print('sides check [not colliding]')
+            
+        # if projection <= radius: 
+        #     print(p, p1, p2, d12, projection)
+        #     return True
+        
+    
+    return False
+
+def is_sprite_intersecting_with_line(start:tuple, end:tuple, sprite:arcade.Sprite):
+    
+    pass
+
+def _check_for_collision(sprite1: Sprite, sprite2: Sprite) -> bool:
+    """
+    Check for collision between two sprites.
+
+    :param Sprite sprite1: Sprite 1
+    :param Sprite sprite2: Sprite 2
+
+    :returns: True if sprites overlap.
+    :rtype: bool
+    """
+    collision_radius_sum = sprite1.collision_radius + sprite2.collision_radius
+
+    diff_x = sprite1.position[0] - sprite2.position[0]
+    diff_x2 = diff_x * diff_x
+
+    if diff_x2 > collision_radius_sum * collision_radius_sum:
+        return False
+
+    diff_y = sprite1.position[1] - sprite2.position[1]
+    diff_y2 = diff_y * diff_y
+    if diff_y2 > collision_radius_sum * collision_radius_sum:
+        return False
+
+    distance = diff_x2 + diff_y2
+    if distance > collision_radius_sum * collision_radius_sum:
+        return False
+
+    ### mash custom starts
+    if getattr(sprite1, 'collides_with_radius', False):
+        if getattr(sprite2, 'collides_with_radius', False):
+            # print('collision case 0')
+            return True
+        else:
+            # print('collision case 1')
+            return is_polygon_intersecting_with_circle(sprite2.get_adjusted_hit_box(), sprite1.position, sprite1.collision_radius)
+    else:
+        if getattr(sprite2, 'collides_with_radius', False):
+            # print('collision case 2')
+            return is_polygon_intersecting_with_circle(sprite1.get_adjusted_hit_box(), sprite2.position, sprite2.collision_radius)
+    ### mash custom ends
+    
+    return are_polygons_intersecting(
+        sprite1.get_adjusted_hit_box(), sprite2.get_adjusted_hit_box()
+    )
+
+### code injection
+arcade.sprite_list.spatial_hash._check_for_collision = _check_for_collision
+
+
 
 
 def load_shader(file_path:str, target_window:arcade.Window, channels:list[arcade.Texture]):
@@ -136,3 +245,27 @@ def reserve_tick_for(tick_func, delay:float, duration:float, interval:float = 1/
         return tick_for(tick_func, duration, interval, *args, **kwargs)
     
     reserve_run(reserved, delay)
+
+def patched_unschedule_in_clock(self, func, *args):
+    
+    if args:
+        valid_items = set(item for item in self._schedule_interval_items if item.func == func and item.args == args)
+    else:
+        valid_items = set(item for item in self._schedule_interval_items if item.func == func)
+
+    if self._current_interval_item:
+        if self._current_interval_item.func == func:
+            valid_items.add(self._current_interval_item)
+    # else: print(f'unschedule : {func} not found')
+
+    for item in valid_items:
+        item.interval = 0
+        item.func = lambda x, *args, **kwargs: x
+
+    self._schedule_items = [i for i in self._schedule_items if i.func != func]
+
+def patched_unschedule(func, *args):
+    pyglet_clock.unschedule(func, *args)
+
+pyglet.clock.Clock.unschedule = patched_unschedule_in_clock
+unschedule = patched_unschedule
